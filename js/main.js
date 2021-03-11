3710c4e0643d4761187278938dfd60125b3f0576
YuHima_03 <yuhima03@gmail.com>
2021-03-11 21:05:12 +0900

    不要な機能を追加してそして削除した

--- js/main.js
+++ js/main.js
@@ -202,52 +202,6 @@ function getAllParents(targetElement){
 }
 
 /**
- * ```targetElement```にCSSで付けられた値を取得 (恐ろしいほどに処理が多いので何度も実行すると重くなる可能性アリ)
- * @param {Element} targetElement 
- * @param {Boolean} getInheritedStyle 継承された値も取得
- * @returns {Object}
- */
-function getStyleDeclarations(targetElement, getInheritedStyle = false, getFromCSSStyleSheet = true){
-    let CSS = document.styleSheets;
-    let result = Object();
-
-    if(getInheritedStyle){
-        [...getAllParents(targetElement), targetElement].forEach(element => {
-            let style = getStyleDeclarations(element, false, getFromCSSStyleSheet);
-            Object.keys(style).forEach(key => {
-                result[key] = style[key];
-            });
-        });
-    }
-    else if(getFromCSSStyleSheet){
-        //スタイルシートにちゃんと書いてあるやつ
-        [...CSS].forEach(value => { //読み込んだCSSごとに
-            [...value.cssRules].forEach(rule => { //CSSの一つの纏まり`{...}`ごとに
-                //セレクターの文字列で検索して合致するかチェックし、合致したらそのスタイルの値を記録していく
-                let elements = document.querySelectorAll(rule.selectorText);
-
-                [...elements].forEach(element => {
-                    if(element === targetElement){
-                        let style = rule.style;
-                        [...style].forEach(styleValue => {
-                            result[styleValue] = style[styleValue];
-                        });
-                    }
-                });
-            });
-        });
-    }
-
-    //直書きされてるやつ
-    let style = targetElement.style;
-    [...style].forEach(value => {
-        result[value] = style[value];
-    });
-
-    return result;
-}
-
-/**
  * ```a≡k (mod b)```の```k(余り)```を返す(正の数)
  * @param {Number} a 
  * @param {Number} b 
@@ -261,111 +215,12 @@ function mod(a, b){
 }
 
 /**
- * スタイルの値を分割して
- * @param {String} styleValue 
- * @returns {Array}
+ * ```str```の単位のみを取り除く
+ * @param {String} str 
+ * @returns {Number}
  */
-function sliceStyleValue(styleValue) {
-    let result = [];
-    [...styleValue.split(/,/)].forEach(value => {
-        let splitValue = value.match(/\S+/g);
-        result.push([...splitValue]);
-    });
-
-    return result;
-}
-
-/**
- * `styleValue` の `index` 番目の値を `replaceValue` に変える
- * @param {String} styleValue 
- * @param {Number|Array} index 1次配列の場合は`,`で区切った[0]番目のなかの[1]番目の値、2次配列の場合は1次配列を複数
- * @param {String} replaceValue 
- * @returns {String}
- */
-function replaceStyleValue(styleValue, index, replaceValue){
-    let result = sliceStyleValue(styleValue);
-
-    let err_index_type = new TypeError("`index` must be Number (or Array)");
-    let err_out_of_range = new RangeError("`index` out of range");
-
-    if(typeof(index) == "number"){
-        result.forEach((arr, i1) => {
-            if(isset(result[i1][index])){
-                result[i1][index] = replaceValue;
-            }
-            else {
-                throw err_out_of_range;
-            }
-        });
-    }
-    else if(index instanceof Array){
-        /**
-         * index が配列の場合...
-         * 以下の3通りの記述が可能
-         * 
-         * ・index[0], index[1] が共に整数 * 
-         * ・index[0], ..., index[n] が全て配列 かつ
-         *      ・index[n][0], index[n][1] が共に整数 *
-         *      ・index[n][0] が整数で index[n][1] が配列 かつ
-         *          ・index[n][1][0], ..., index[n][1][m] が全て整数 *
-         */
-
-        index.forEach(v => { // index[n]<Array> (ただしn = i)
-            if(v.length == 2 && typeof(v[0]) == "number"){
-                //result[index[i][0]][index[i][1]] を置き換え(v = index[i])
-                if(v[0] < result.length){
-                    if(typeof(v[1]) == "number"){
-                        if(v[1] < result[v[0]].length){
-                            result[v[0]][v[1]] = replaceValue;
-                        }
-                        else{
-                            throw err_out_of_range;
-                        }
-                    }
-                    else if(v[1] instanceof Array){
-                        v[1].forEach(v2 => {
-                            if(typeof(v2) == "number"){
-                                if(v2 < result[v[0]].length){
-                                    result[v[0]][v2] = replaceValue;
-                                }
-                                else{
-                                    throw err_out_of_range;
-                                }
-                            }
-                            else{
-                                throw err_index_type;
-                            }
-                        });
-                    }
-                    else{
-                        throw err_index_type;
-                    }
-                }
-                else{
-                    throw err_out_of_range;
-                }
-            }
-            else{
-                throw err_index_type;
-            }
-        });
-    }
-    else{
-        throw err_index_type;
-    }
-
-    //配列から文字列に変換
-    let resultStr = String();
-    
-    result.forEach(v1 => {
-        let tmp = String();
-        v1.forEach(v2 => {
-            tmp += ("\x20" + v2);
-        });
-        resultStr += ("," + tmp);
-    });
-
-    return resultStr.slice(2);
+function removeUnit(str){
+    return Number(str.match(/[\d\.]*/)[0]);
 }
 
 /// DOMツリー読み込み後実行 ///
